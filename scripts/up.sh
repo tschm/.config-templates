ec6242bd73cf9bf7bfe7560023cb0ea31c4bbd24
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-19 22:44:09 +0200

    no need to remove the script in the sync workflow

--- scripts/up.sh
+++ scripts/up.sh
@@ -14,22 +14,11 @@ main() {
   rm -f templates.zip
 
   EXTRACTED_DIR="${TEMP_DIR}/.config-templates-main"
-  #[ -d "$EXTRACTED_DIR" ] || exit 1
-
-  #updated=0
+  # don't copy the script itself into the repo
+  rm "${TEMP_DIR}/.config-templates-main/scripts/up.sh"
   cp -Rf $EXTRACTED_DIR/. .
+
   git status
-
-  #while IFS= read -r -d '' file; do
-  #  target_file="./${file#$EXTRACTED_DIR/}"
-  #  mkdir -p "$(dirname "$target_file")"
-  #  #if ! cmp -s "$file" "$target_file"; then
-  #  cp -v "$file" "$target_file"
-  #  #((updated++))
-  #  #fi
-  #done < <(find "$EXTRACTED_DIR" -type f -print0)
-
-  #echo "$updated files updated."
 }
 trap 'rm -rf "$TEMP_DIR"' EXIT
 main "$@"
