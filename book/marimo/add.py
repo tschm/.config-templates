97f23d1c6340759e845a5bc1bff3f45b9d3f6072
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-11-16 15:26:49 +0400

    fmt

--- book/marimo/add.py
+++ book/marimo/add.py
@@ -1,3 +1,11 @@
+"""Marimo app notebook demonstrating the config.add function.
+
+This module is an auto-generated Marimo notebook. It imports the add helper
+from the config package and evaluates a few simple cells. The module is used
+for documentation/book integration and can be launched with `python -m marimo`
+or by running this file directly.
+"""
+
 import marimo
 
 __generated_with = "0.17.7"
@@ -7,12 +15,13 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     from config import add
+
     return (add,)
 
 
 @app.cell
 def _(add):
-    add(2,3)
+    add(2, 3)
     return
 
 
