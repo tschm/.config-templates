faa33d323eb82495de1e5d8f6c535456c5960b82
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-11-25 08:36:20 +0400

    add passing test

--- tests/test_pseudo2.py
+++ tests/test_pseudo2.py
@@ -1,2 +1,5 @@
+"""Trivial test module to satisfy docstring linting (D100)."""
+
 def test_pseudo2():
+    """Ensure the test suite is wired correctly by asserting truthiness (D103)."""
     assert True
